19ebb5fc8698261429ded97517a4e6dfdcad7c49
gibbz00 <gabrielhansson@gmail.com>
2023-09-20 20:45:19 +0200

    `Transform` support for `MultiPoint`.

--- src/adaptors/geo_types.rs
+++ src/adaptors/geo_types.rs
@@ -1,4 +1,4 @@
-use geo_types::Point;
+use geo_types::geometry::*;
 
 use crate::transform::Transform;
 
@@ -13,6 +13,16 @@ impl Transform for Point {
         self.set_y(xy.1);
 
         Ok(())
+    }
+}
+
+impl Transform for MultiPoint {
+    fn transform_coordinates<F>(&mut self, f: &mut F) -> crate::errors::Result<()>
+    where
+        F: FnMut(f64, f64, f64) -> crate::errors::Result<(f64, f64, f64)>,
+    {
+        self.iter_mut()
+            .try_for_each(|point| point.transform_coordinates(f))
     }
 }
 
@@ -36,5 +46,22 @@ mod tests {
 
         assert_abs_diff_eq!(point.x(), 222650.79679758527, epsilon = 1.0e-10);
         assert_abs_diff_eq!(point.y(), 110642.22941193319, epsilon = 1.0e-10);
+    }
+
+    #[test]
+    fn transforms_multi_point() {
+        let mut multi_point: MultiPoint = (0..10)
+            .map(|_| Point::from((2.0f64.to_radians(), 1.0f64.to_radians())))
+            .collect();
+
+        let from = Proj::from_proj_string("+proj=latlong +ellps=GRS80").unwrap();
+        let to = Proj::from_proj_string("+proj=etmerc +ellps=GRS80").unwrap();
+
+        transform(&from, &to, &mut multi_point).unwrap();
+
+        multi_point.iter().for_each(|point| {
+            assert_abs_diff_eq!(point.x(), 222650.79679758527, epsilon = 1.0e-10);
+            assert_abs_diff_eq!(point.y(), 110642.22941193319, epsilon = 1.0e-10);
+        });
     }
 }
