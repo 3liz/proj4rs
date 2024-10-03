0c3c6b3b23d17ebb92acf331bc4dd33ca520f4cd
David Marteau <dmarteau@3liz.com>
2024-10-03 13:02:04 +0200

    Fix code formatting

--- src/projections/laea.rs
+++ src/projections/laea.rs
@@ -455,25 +455,21 @@ mod tests {
 
     #[test]
     fn test_epsg3035() {
-        // cf https://github.com/3liz/proj4rs/issues/18   
-        
+        // cf https://github.com/3liz/proj4rs/issues/18
+
         let p = Proj::from_proj_string(
-            "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80"
-        ).unwrap();
+            "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80",
+        )
+        .unwrap();
 
         println!("{:#?}", p.projection());
 
-        let inputs = [
-            (
-                (15.4213696, 47.0766716, 0.),
-                (4732659.007426266, 2677630.7269610995, 0.),
-            ),
-        ];
+        let inputs = [(
+            (15.4213696, 47.0766716, 0.),
+            (4732659.007426266, 2677630.7269610995, 0.),
+        )];
 
         test_proj_forward(&p, &inputs, EPS_10);
         test_proj_inverse(&p, &inputs, 1.0e-7);
- 
-
- 
     }
 }
